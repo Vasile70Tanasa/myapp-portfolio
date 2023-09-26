7ba787d7ec5e3fb913fef43d64b56cca6c60cc24
Vasile Tanasa <vasiletanasa@gmail.com>
2023-09-26 20:38:52 +0300

    Remove a hardcoded Django secret key

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+
+import os
+
 django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x(b^d1_90*3v&6sn&hd*lm07r3-1v=pmw^jza!$4n2m5#wgt*b"
+# SECRET_KEY = "django-insecure-x(b^d1_90*3v&6sn&hd*lm07r3-1v=pmw^jza!$4n2m5#wgt*b"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -114,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = "static/"
 
