a10e2452dcbb96f48a0e0330480b40877a5d4a96
Vasile Tanasa <vasiletanasa@gmail.com>
2023-10-04 14:02:28 +0300

    second commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -15,7 +15,6 @@ import os
 import dj_database_url
 import django_heroku
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django_heroku",
 ]
 
 MIDDLEWARE = [
